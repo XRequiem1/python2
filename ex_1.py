5c2ab0d3c696f4be178b01392a14c6f37bdd2db3
XRequiem1 <144215937+XRequiem1@users.noreply.github.com>
2023-10-04 13:44:08 +0300

    Add files via upload

---
+++ ex_1.py
@@ -0,0 +1,112 @@
+#срезы
+#s = input()
+#print(s[2])
+#print(s[-1])
+#print(s[:5])
+
+#количество слов
+#s = input()
+#k = s.count(' ')
+#print(k+1)
+
+#две половинки
+#s = input()
+#n = len(s) + 1
+#w = s[0:1//2]
+#ww = s[1//2:0]
+#print(w,ww)
+
+#первое и последнее вхождение
+#s = input()
+#print(s.find('f'))
+#print(s.rfind('f'))
+
+#удаление фрагмента
+#s = input()
+#n = s[:s.find('h')] + s[s.rfind('h') + 1:]
+#print(n)
+
+#замена подстроки
+#s = str(input())
+#print(s.replace('1', 'one' ))
+
+#удаление символа
+#s = str(input())
+#print(s.replace('@', ''))
+
+#замена внутри фрагмента
+#s = input()
+#w = s[:s.find('h') + 1]
+#f = s[s.find('h') + 1:s.rfind('h')]
+#x = s[s.rfind('h'):]
+#s = w + f.replace('h', 'H') + x
+#print(s)
+
+#минимум из двух чисел
+#x = int(input())
+#y = int(input())
+#if x > y:
+#    print(y)
+#elif x < y:
+#    print(x)
+
+#знак числа
+#x = int(input())
+#if x > 0:
+#    print(1)
+#elif x < 0:
+#    print(-1)
+#elif x == 0:
+#    print(0)
+
+#шахматная доска
+#x = int(input())
+#y = int(input())
+#w = int(input())
+#e = int(input())
+#s = x + y + w + e
+#if s % 2 == 0:
+#    print('YES')
+#else:
+#    print('NO')
+
+#високосный год
+#s = int(input())
+#if s % 4 == 0  and s % 100 != 0 or s % 400 == 0:
+#    print('YES')
+#else:
+#    print('NO')
+
+#сколько совпадает чисел
+#q = int(input())
+#w = int(input())
+#e = int(input())
+#if q == w == e:
+#    print(3)
+#elif q == w or q == e or w == e:
+#    print(2)
+#else:
+#    print(0)
+
+#ход ладьи
+#x = int(input())
+#y = int(input())
+#w = int(input())
+#e = int(input())
+#if x == w or y == e:
+#    print('YES')
+#else:
+#    print('NO')
+
+#шоколадка
+#n = int(input())
+#m = int(input())
+#k = int(input())
+#if k == m or k == n:
+#    print('YES')
+#else:
+#    print('NO')
+
+
+
+
